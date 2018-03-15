c9d4c4b6e5a449bc05408ddcb3439f2a1f28be42
TWSK, aka. devTWSK <terencewu.tw@gmail.com>
2018-03-15 13:42:34 +0800

    Improve numerical accuracy

--- sechpulse.m
+++ sechpulse.m
@@ -1,24 +1,23 @@
-function u = sechpulse(t,t0,FWHM,P0,C)
+function u = sechpulse(t,tc,T0,P0,C)
   
 % This function computes a hyperbolic secant pulse with the
 % specified parameters:
 %
 %   sqrt(P0)*sech((t-t0)/T)*exp{-i(C/2)*[(t-t0)/T]^2}
-%     where T = FWHM / (2*acosh(sqrt(2)))
 % 
 % USAGE:
 % 
 % u = sechpulse (t);
-% u = sechpulse (t,t0);
-% u = sechpulse (t,t0,FWHM);
-% u = sechpulse (t,t0,FWHM,P0);
-% u = sechpulse (t,t0,FWHM,P0,C);
+% u = sechpulse (t,tc);
+% u = sechpulse (t,tc,T0);
+% u = sechpulse (t,tc,T0,P0);
+% u = sechpulse (t,tc,T0,P0,C);
 % 
 % INPUT:
 % 
 % t     vector of times at which to compute u
-% t0    center of pulse (default = 0)
-% FWHM  full-width at half-intensity of pulse (default = 1)
+% tc    center of pulse (default = 0)
+% T0    half width at 1/e intensity of pulse (default = 1)
 % P0    peak intensity (|u|^2 at t=t0) of pulse (default = 1)
 % C     chirp parameter (default = 0)
 % 
@@ -28,18 +27,19 @@ function u = sechpulse(t,t0,FWHM,P0,C)
 %       amplitude
 
   
-if (nargin<5)
+if (nargin < 5)
   C = 0;
 end
-if (nargin<4)
+if (nargin < 4)
   P0 = 1;
 end
-if (nargin<3)
-  FWHM = 1;
+if (nargin < 3)
+  T0 = 1;
 end
-if (nargin<2)
-  t0 = 0;
+if (nargin < 2)
+  tc = 0;
 end
 
-T0 = FWHM/(2*acosh(sqrt(2)));
-u = sqrt(P0)*sech((t-t0)/T0).*exp(-i*C*(t-t0).^2/(2*T0^2));
+% T0 = T0/(2*acosh(sqrt(2)));
+% u = sqrt(P0)*sech((t-tc)/T0).*exp(-i*C*(t-tc).^2/(2*T0^2));
+u = sqrt(P0) .* sech((t-tc)/T0) .* exp(-1j * C * (t-tc).^2 / (2 * T0^2));
