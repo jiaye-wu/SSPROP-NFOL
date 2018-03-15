96ef3ac9bd161c260419767de03e17fafede3908
TWSK, aka. devTWSK <terencewu.tw@gmail.com>
2018-03-15 13:38:24 +0800

    New algorithm using T0 instead of FWHM
    In order to solve numerical issues...

--- gaussian.m
+++ gaussian.m
@@ -1,25 +1,24 @@
-function u = gaussian(t,t0,FWHM,P0,m,C)
+function u = gaussian(t,tc,T0,P0,m,C)
   
 % This function computes a gaussian or supergaussian pulse with
 % the specified parameters:
 %
 %   sqrt(P0) * exp{ -[(1+iC)/2] * [(t-t0)/T]^(2m) }
-%      where T = FWHM/2*sqrt(log(2))
 % 
 % USAGE:
 % 
 % u = gaussian (t);
-% u = gaussian (t,t0);
-% u = gaussian (t,t0,FWHM);
-% u = gaussian (t,t0,FWHM,P0);
-% u = gaussian (t,t0,FWHM,P0,m);
-% u = gaussian (t,t0,FWHM,P0,m,C);
+% u = gaussian (t,tc);
+% u = gaussian (t,tc,T0);
+% u = gaussian (t,tc,T0,P0);
+% u = gaussian (t,tc,T0,P0,m);
+% u = gaussian (t,tc,T0,P0,m,C);
 % 
 % INPUT:
 % 
 % t     vector of times at which to compute u
-% t0    center of pulse (default = 0)
-% FWHM  full-width at half-intensity of pulse (default = 1)
+% tc    center of pulse (default = 0)
+% T0    half width at 1/e intensity of pulse (default = 1)
 % P0    peak intensity (|u|^2 at t=t0) of pulse (default = 1)
 % m     Gaussian order (default = 1)
 % C     chirp parameter (default = 0)
@@ -29,28 +28,23 @@ function u = gaussian(t,t0,FWHM,P0,m,C)
 % u     vector of the same size as t, representing pulse
 %       amplitude
 %
-% NOTES:
-%
-% In order to produce a standard gaussian pulse of the form
-% exp(-t.^2/2), you can use:  
-%
-% u = gaussian(t,0,2*sqrt(log(2)));
   
-if (nargin<6)
+if (nargin < 6)
   C = 0;
 end
-if (nargin<5)
+if (nargin < 5)
   m = 1;
 end
-if (nargin<4)
+if (nargin < 4)
   P0 = 1;
 end
-if (nargin<3)
-  FWHM = 1;
+if (nargin < 3)
+  T0 = 1;
 end
-if (nargin<2)
-  t0 = 0;
+if (nargin < 2)
+  tc = 0;
 end
 
 % u = sqrt(P0)*pow2(-((1+i*C)/2)*(2*(t-t0)/FWHM).^(2*m));
-u = sqrt(P0)*exp(-((1+i*C)/2)*(2*(t-t0)/FWHM).^(2*m));
+% u = sqrt(P0)*exp(-((1+i*C)/2)*(2*(t-t0)/T0).^(2*m));
+u = sqrt(P0) .* exp(-((1 + 1j*C) / 2) .* ((t-tc) ./ T0).^(2*m));
